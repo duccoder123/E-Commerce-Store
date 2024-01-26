49b3f257916d8eac1120948e0cead7e6a2e429c4
dang2303 <8duchuynh@gmail.com>
2024-01-26 13:37:42 +0700

    createAPIandController

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+        public ProductsController(StoreDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts (){
+            var productList = await  _context.Products.ToListAsync();
+            return Ok(productList);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id){
+            var product =await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(product);
+        }
+    }
+}
