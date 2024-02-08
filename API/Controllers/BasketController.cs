89889c114ac47f26b3defbcb582ea1946ca18584
dang2303 <8duchuynh@gmail.com>
2024-02-08 20:11:52 +0700

    create removing basket item

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,11 +14,22 @@ namespace API.Controllers
         _context = context;
         }
         [HttpGet]
-        public async Task<ActionResult<Basket>> GetBasket()
+        public async Task<ActionResult<BasketDto>> GetBasket()
         {
             var basket = await RetrieveBasket();
             if (basket == null) return NotFound();
-            return basket;
+            return new BasketDto{
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDto{
+                    ProductId = item.ProductId,
+                     Name = item.Product.Name,
+                     Price = item.Product.Price,
+                     PictureUrl = item.Product.PictureUrl,
+                     Type = item.Product.Type,
+                     Quantity = item.Quantity
+                }).ToList()
+            } ;
         }
         [HttpPost]
         public async Task<ActionResult> AddItemToBasket(int productId, int quantity){
@@ -32,7 +44,12 @@ namespace API.Controllers
         }
         [HttpDelete]
         public async Task<ActionResult> RemoveBasketItem(int productId, int quantity){
-
+            var basket = await RetrieveBasket();
+            if(basket == null) return NotFound();
+            basket.RemoveItem(productId,quantity);
+            var result = await _context.SaveChangesAsync() > 0;
+            if(result) return Ok();
+            return BadRequest(new ProblemDetails{Title="Problem removing item from the basket"});
         }
             private async Task<Basket> RetrieveBasket()
         {
